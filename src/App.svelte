023db10c72593b07b907e5347da4b4cb99f63d25
Joshua Evans <thejoshuaevans@gmail.com>
2020-04-18 00:03:07 -0700

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<head>
+	<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<style>
+div.app-container {
+	font-family: 'Rubik', sans-serif;
+}
+</style> 
+
+<script>
+import STATES from '../lib/utils/states.js';
+import EnterCredentials from './Components/EnterCredentials.svelte';
+import ChoosePool from './Components/ChoosePool.svelte';
+import ListUsers from './Components/ListUsers.svelte';
+
+$: state = STATES.ENTER_CREDENTIALS;
+
+const sharedData = {
+	test: 'test success!'
+};
+
+function handleChangeState(event) {
+	state = event.detail.newState;
+}
+</script>
+
+<div class="app-container">
+	<h1>Simply Cognito</h1>
+	<p>This system is designed to help manage users in your cognito user pools. </p>
+	{#if state === STATES.ENTER_CREDENTIALS}
+		<EnterCredentials on:changeState={handleChangeState} {sharedData}/>
+	{:else if state === STATES.CHOOSE_POOL}
+		<ChoosePool on:changeState={handleChangeState} {sharedData}/>
+	{:else if state === STATES.LIST_USERS}
+		<ListUsers on:changeState={handleChangeState} {sharedData}/>
+	{/if}
+</div>
---
+++ src/Components/EnterCredentials.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  import STATES from '../../lib/utils/states.js';
+  
+  export let sharedData;
+
+  const dispatch = createEventDispatcher();
+
+  sharedData.setTest = 'set successful!';
+
+  function changeState() {
+    dispatch('changeState', {
+      newState: STATES.CHOOSE_POOL
+    });
+  }
+
+  function handleCredentialFormSubmit(event) {
+    event.preventDefault();
+    console.log(this.elements.id.value);
+    console.log(this.elements.secret.value);
+    console.log(event);
+  }
+</script>
+
+<div class='enter-credentials-container'>
+  <h2>Enter AWS Credentials</h2>
+  <form on:submit={handleCredentialFormSubmit}>
+    <div class='credential-input'>
+      <label>
+        Access Key ID: <input type='text' name='id'> 
+      </label>
+    </div>
+    <div class = 'credential-input'>
+      <label>
+        Access Key Secret: <input type='password' name='secret'>      
+      </label>
+    </div>
+    <div class = 'credential-input'>
+      <button type='submit'>Submit</button>
+    </div>
+  </form>
+</div>
